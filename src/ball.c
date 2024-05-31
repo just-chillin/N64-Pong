1a77ed94cbf9789905e1b1801a3b6d05ca6dcce7
Corrina Sivak <csivak345@gmail.com>
2024-05-31 11:31:31 -0400

    Ball rendering

--- src/ball.c
+++ src/ball.c
@@ -1,5 +1,7 @@
 #include "ball.h"
 
+const int SIZE = 10;
+
 struct ball make_ball() {
     // Determine the center of the screen
     int center_x = display_get_width() / 2;
@@ -11,8 +13,8 @@ struct ball make_ball() {
     return ball;
 }
 
-void render_ball(surface_t* display) {
-
+void render_ball(struct ball* ball, surface_t* display) {
+    graphics_draw_box(display, ball->x, ball->y, SIZE, SIZE, graphics_make_color(255, 255, 255, 255));
 }
 
 void update_ball(struct ball* ball, surface_t* display) {
@@ -21,8 +23,5 @@ void update_ball(struct ball* ball, surface_t* display) {
     // Check for collisions
     // Update the vector based on the collisions
     // Draw the ball
-
-    // print the address of display
-    printf("display address: %p\n", display);
-    render_ball(display);
+    render_ball(ball, display);
 }
